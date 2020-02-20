13286d3eafd0927078c18271a63c2979bce76662
kimgyuri <kkr6495@naver.com>
2020-02-20 17:07:19 +0900

    [GR] cafe 기능 구현 완료함

--- SecondTest/src/main/java/com/dowon/project/cafe/dao/CafeDaoImpl.java
+++ SecondTest/src/main/java/com/dowon/project/cafe/dao/CafeDaoImpl.java
@@ -13,5 +13,54 @@ public class CafeDaoImpl implements CafeDao{
 	@Autowired
 	private SqlSession session;
 	
+	@Override
+	public int getCount(CafeDto dto) {
+		return session.selectOne("cafe.getCount",dto);
+	}
+
+	@Override
+	public List<CafeDto> getList(CafeDto dto) {
+		List<CafeDto> list= session.selectList("cafe.getList",dto);
+		return list;
+	}
+
+	@Override
+	public void insert(CafeDto dto) {
+		session.insert("cafe.insert",dto);		
+	}
+
+	@Override
+	public void delete(int num) {
+		session.delete("cafe.delete",num);
+	}
+
+	@Override
+	public CafeDto getData(int num) {
+		CafeDto dto=session.selectOne("cafe.getData2",num);
+		return dto;
+		
+	}
+
+	@Override
+	public void update(CafeDto dto) {
+		session.update("cafe.update",dto);
+	}
+
+	@Override
+	public void addViewCount(int num) {
+		session.update("cafe.addViewCount",num);
+	}
+
+	@Override
+	public void commUpdate(CafeDto dto) {
+		session.update("cafe.commupdate",dto);	
+		
+	}
+
+	@Override
+	public CafeDto getDate(CafeDto dto) {
+		return session.selectOne("cafe.getData1", dto);
+	}
+
 	
 }
