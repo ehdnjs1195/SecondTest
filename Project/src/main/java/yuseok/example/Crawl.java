4d5db9c8ada82ed565f55a4f7ec449f41d496d8e
yuseok <znql16@gmail.com>
2020-03-20 18:17:26 +0900

    [yuSeok] 다음에서 TOP20 순위, 이미지, 제목, 별점, 가져오는 크롤링 코드 작성

--- Project/src/main/java/yuseok/example/Crawl.java
+++ Project/src/main/java/yuseok/example/Crawl.java
@@ -21,25 +21,31 @@ public class Crawl{
          * select 를 이용하여 원하는 태그를 선택한다. select 는 원하는 값을 가져오기 위한 중요한 기능이다.
          * ==>원하는 값들이 들어있는 덩어리를 가져온다.
          */
-        Elements element  = doc.select("div.obj_section");
+        Elements element  = doc.select(".lst_detail_t1 li");
         
 		//Iterator을 사용하여 하나씩 값 가져오기
-		Iterator<Element> title = element.select("dt.tit a").iterator();		        
-		Iterator<Element> ie = element.select("span.num").iterator();		        
-		Iterator<Element> ie2 = element.select("span.num2").iterator();		        
-		Iterator<Element> ie3 = element.select("dl.info_exp").iterator();		        
+//		Iterator<Element> image = element.select(".lst_detail_t1 .thumb img").iterator(); //이미지		        
+		Iterator<Element> title = element.select(".lst_dsc .tit a").iterator(); //제목
+		Iterator<Element> ie = element.select(".star_t1 .num").iterator();//네티즌 평점	        
+		Iterator<Element> ie2 = element.select(".star_t1 .num2 em").iterator();		        
+//		Iterator<Element> ie3 = element.select(".lst_dsc .info_exp dd .star_t1 .num").iterator();		        
 		Iterator<Element> ie4 = element.select(".lst_dsc .info_txt1 .tit_t1+dd").iterator();		        
 		Iterator<Element> ie5 = element.select(".lst_dsc .info_txt1 .tit_t2+dd a").iterator();        
-		Iterator<Element> ie6 = element.select(".lst_dsc .info_txt1 .tit_t3+dd").iterator();        
-		while (ie.hasNext()) {
+//		Iterator<Element> ie6 = element.select(".lst_dsc .info_txt1 .tit_t3+dd").iterator();
+		
+		int count=0;
+		while (title.hasNext()) {
+			count = count+1;
 			System.out.println(
-					"제목 : "+title.next().text() +"|"+
-					"네티즌 : "+ie.next().text() +"|"+
-					ie2.next().text() +"|"+
-					ie3.next().text() +"|"+
-					"개요 : "+ie4.next().text() +"|"+
-					"감독 : "+ie5.next().text() +"|"+
-					"출연 : "+ie6.next().text());
+					"순위 :"+count+"위" +"|"+
+//					"이미지 :"+image.next().text() +"|"+
+					"제목 :"+title.next().text() +"|"+
+					"네티즌 평점 :"+ie.next().text() +"|"+
+					"참여 :"+ie2.next().text()+"명" +"|"+
+//					"예매율 :"+ie3.next().text()+"%" +"|"+
+					"개요 :"+ie4.next().text() +"|"+
+					"감독 :"+ie5.next().text() +"|");
+//					"출연 :"+ie6.next().text());
 		}
 
 	}//main
---
+++ Project/src/main/webapp/WEB-INF/views/currentMovie.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Current screening movie</title>
+</head>
+<body>
+	<h1>현재 상영영화</h1>
+	
+</body>
+</html>
